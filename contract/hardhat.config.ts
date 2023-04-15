c9fe9f5d856ba18d0d9d30912dde25d2b0e5cde9
Jacob <encody@users.noreply.github.com>
2023-04-16 04:02:17 +0900

    deploy script and prettier config

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -1,8 +1,25 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+require('dotenv').config();
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: {
+    version: '0.8.18',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  defaultNetwork: 'mumbai',
+  networks: {
+    mumbai: {
+      chainId: 80001,
+      url: process.env.MUMBAI_URL!,
+      accounts: [process.env.PRIVATE_KEY!],
+    },
+  },
 };
 
 export default config;
